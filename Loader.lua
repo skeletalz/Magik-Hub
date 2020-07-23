a94e2d18e4f3a3db28ac46cc5c2ea891ef1ff71b
SlaxxF <61214227+skeletalz@users.noreply.github.com>
2020-07-23 08:18:08 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,6 +4,7 @@ local files = {
   notif = "https://raw.githubusercontent.com/skeletalz/Main-Stuff/master/Notification%20Service"
 }
 loadstring(game:HttpGet(list.games))()
+loadstring(game:HttpGet(list.notif))()
 Notification("Magik Hub","Magik Hub is being loaded!",{Enter = 0.6,Exit = 0.8})
 wait(1.2)
 local not = game.Players.LocalPlayer:WaitForChild("PlayerGui")["Notification"]
@@ -13,9 +14,24 @@ game:GetService("TweenService"):Create(not.Scroll.News.Title,TweenInfo.new(0.8),
 game:GetService("TweenService"):Create(not.Scroll.News.Desc, TweenInfo.new(0.8),{TextTransparency = 1}):Play() 
 wait(0.8)
 not:Destroy()
-
+local ingame = false
 for i,gam in pairs(_G.Games) do
   if game.PlaceId == gam.ID then
+     ingame = true
      loadstring(game:HttpGet(list.game..gam.Script))()
+ else
+     ingame = false
   end
+end
+if ingame == false then
+  loadstring(game:HttpGet(list.notif))()
+  Notification("Magik Hub","Your not in a supported game!",{Enter = 0.6,Exit = 0.8})
+  wait(1.2)
+  local not = game.Players.LocalPlayer:WaitForChild("PlayerGui")["Notification"]
+  game:GetService("TweenService"):Create(not.Scroll.News,TweenInfo.new(0.8),{ImageTransparency = 1}):Play()
+  game:GetService("TweenService"):Create(not.Scroll.News.Close,TweenInfo.new(0.8),{ImageTransparency = 1}):Play()
+  game:GetService("TweenService"):Create(not.Scroll.News.Title,TweenInfo.new(0.8),{TextTransparency = 1}):Play()
+  game:GetService("TweenService"):Create(not.Scroll.News.Desc, TweenInfo.new(0.8),{TextTransparency = 1}):Play() 
+  wait(0.8)
+  not:Destroy()
 end
