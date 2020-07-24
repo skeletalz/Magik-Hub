79020bc39561cc916bd840fe31c2d1a84a719bbf
SlaxxF <61214227+skeletalz@users.noreply.github.com>
2020-07-24 12:08:49 -0400

    Update GamesList.lua

--- GamesList.lua
+++ GamesList.lua
@@ -1,3 +1,4 @@
 _G.Games = {
- ["5405488930"] = {Name = "Test place" , Script = "test.lua",ID = 5405488930,Status = 4}
+ ["5405488930"] = {Name = "Test place" , Script = "test.lua",ID = 5405488930,Status = 4},
+ ["4623386862"] = {Name = "Piggy", Script = "Piggy.lua",ID = 4623386862,Status = 4}
 }
