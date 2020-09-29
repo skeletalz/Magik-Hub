babb00b69458dbc9cd09957a91a8386a6c19ca70
SlaxxF <61214227+skeletalz@users.noreply.github.com>
2020-09-29 11:48:34 -0400

    Update GamesList.lua

--- GamesList.lua
+++ GamesList.lua
@@ -2,5 +2,6 @@ _G.Games = {
  ["4623386862"] = {Name = "Piggy", Script = "Piggy.lua",ID = 4623386862,Status = 4},
  ["13822889"] =   {Name = "Lumber Tycoon 2", Script = "LT2.lua",ID = 13822889,Status = 4},
  ["2768379856"] = {Name = "SCP 3008", Script = "3008.lua",ID = 2768379856,Status = 4},
- ["286090429"] = {Name = "Arsenal", Script = "Arsenal.lua",ID= 286090429 ,Status = 4}
+ ["286090429"] = {Name = "Arsenal", Script = "Arsenal.lua",ID= 286090429 ,Status = 4},
+ ["537413528"] = {Name = "Build A Boat", Script = "BABFT.lua", ID= 537413528, Status = 4}
 }
